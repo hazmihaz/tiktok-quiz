1dfb1da4cab139c2b60e26de0ecf53177019c9cb
Hazmi Hafiz <hazmihafiz@gmail.com>
2021-01-31 22:01:53 +0700

    feat: add play button

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -5,7 +5,23 @@
         </video>
         <div
             class="absolute w-full h-full top-0 left-0 flex justify-center items-center"
-        ></div>
+        >
+            <div v-if="!playing" class="opacity-50">
+                <svg
+                    width="64"
+                    height="64"
+                    viewBox="0 0 36 36"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M27.75 18.799L12 27.8923C11 28.4697 9.75 27.748 9.75 26.5933L9.75 8.40673C9.75 7.25203 11 6.53034 12 7.10769L27.75 16.201C28.75 16.7783 28.75 18.2217 27.75 18.799Z"
+                        fill="white"
+                        stroke="white"
+                    />
+                </svg>
+            </div>
+        </div>
     </div>
 </template>
 
